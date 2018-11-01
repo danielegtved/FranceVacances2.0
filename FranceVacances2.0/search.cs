0e39e213148e77cc18fc560270b968df57bfa453
danielegtved <43374944+danielegtved@users.noreply.github.com>
2018-11-01 12:27:08 +0100

    Made search class

---
+++ FranceVacances2.0/search.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FranceVacances2._0
+{
+    class search
+    {
+        private string _searchBar;
+        private string _constantSearch = "search";
+
+        private void Search()
+        {
+
+        }
+
+        private void Compare()
+        {
+
+        }
+    }
+}
